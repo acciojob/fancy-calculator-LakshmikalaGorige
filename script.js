ae00438398a712021e7544022687942f39a4f1c2
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-11-26 21:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let a=prompt("Enter first number");
+let res=parseInt(a)*2;
+let b=prompt("Enter the second number");
+res=res+parseInt(b);
+alert("The final result is: " + res);
